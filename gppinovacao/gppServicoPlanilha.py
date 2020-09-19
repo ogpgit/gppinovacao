27e2de86bcfebf9da51a30f4a4d4f1b08fd9dda1
ogpgit <osvaldogpires@gmail.com>
2020-09-19 07:49:28 -0300

    Implementando a função Abrir Planilha

--- gppinovacao/gppServicoPlanilha.py
+++ gppinovacao/gppServicoPlanilha.py
@@ -3,34 +3,21 @@ from openpyxl import Workbook, open, load_workbook
 
 def abrir_planilha(nome_planilha):
     """
-    Editar e alterar uma planilha excel
+    Abrir uma planilha excel existente
 
-    :param nome_planilha: str com o nome do arquivo excel
-    :return: str de "sucesso" ou "cancelado"
+    :param nome_planilha: str com o nome da planilha
+    :return: um objeto workbook ativado
     """
     nome_planilha = f'G:\AmbientePython\PycharmProjects\gppinovacao\{nome_planilha}'
 
-    ws = load_workbook(nome_planilha,read_only=False)
-    # ws = open(nome_planilha, read_only='n')
-
-    # workbook = load_workbook('----------/dataset.xlsx')
-    # sheet = workbook.active
-    # row_count = sheet.max_row
-    # for i in range(row_count):
-    #    print(sheet.cell(row=i, column=2).value)
-    sheet = ws.active
-    row_count = sheet.max_row
-    print(row_count)
-    for i in range(row_count):
-        print(i)
-        print(sheet.cell(row=i + 1, column=1).value)
-
-
-
-    return 'Sucesso'
+    return load_workbook(nome_planilha).active
 
 if __name__ == '__main__':
-    if abrir_planilha('Contato.xlsx') == 'Sucesso':
-        print('hello')
-    else:
-        print('Arquivo não existe')
+    # abrir_planilha('Contato.xlsx') == 'Sucesso':
+    planilha = abrir_planilha('Contato.xlsx')
+    # planilha = abrir_planilha('gppInovacao.xlsx')
+
+    # print(type(planilha))
+
+    # mostrar o título da planilha
+    print(planilha.title)
