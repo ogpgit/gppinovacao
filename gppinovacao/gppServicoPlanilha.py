cdf9b995a0f944886bcb1bd161ee5f788360b0ae
ogpgit <osvaldogpires@gmail.com>
2020-09-19 08:40:21 -0300

    Implementando funções de Planilha

--- gppinovacao/gppServicoPlanilha.py
+++ gppinovacao/gppServicoPlanilha.py
@@ -12,6 +12,19 @@ def abrir_planilha(nome_planilha):
 
     return load_workbook(nome_planilha).active
 
+def mostrar_conteudo_planilha(planilha):
+    qtdeMaximaLinha = planilha.max_row
+    qtdeMaximaColuna = planilha.max_column
+    for i in range(1, qtdeMaximaLinha + 1):
+        lista = []
+        for j in range(1, qtdeMaximaColuna + 1):
+            lista.append(planilha.cell(row=i, column=j).value)
+        print(lista)
+            # print(planilha.cell(row=i, column=j).value, end="\n")
+
+
+
+
 if __name__ == '__main__':
     # abrir_planilha('Contato.xlsx') == 'Sucesso':
     planilha = abrir_planilha('Contato.xlsx')
@@ -20,4 +33,9 @@ if __name__ == '__main__':
     # print(type(planilha))
 
     # mostrar o título da planilha
+    print('\nTitulo da planilha')
     print(planilha.title)
+
+    # mostrar conteudo da planilha
+    print('\nMostrar conteudo da planilha')
+    mostrar_conteudo_planilha(planilha)
