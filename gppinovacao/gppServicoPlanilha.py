601a0992c9d2cebce31eddc464d3ec53218a089f
ogpgit <osvaldogpires@gmail.com>
2020-09-21 06:32:46 -0300

    Implementando funções de Planilha e Integração contínua com travis CI
    close #7

--- gppinovacao/gppServicoPlanilha.py
+++ gppinovacao/gppServicoPlanilha.py
@@ -2,38 +2,9 @@ from openpyxl import load_workbook
 
 
 def abrir_planilha(nome_planilha):
-    """
-    Abrir uma planilha excel existente
-
-    :param nome_planilha: str com o nome da planilha
-    :return: um objeto workbook ativado
-    """
     nome_planilha = f'G:\AmbientePython\PycharmProjects\gppinovacao\{nome_planilha}'
-
     return load_workbook(nome_planilha).active
 
-def mostrar_conteudo_planilha(planilha):
-    qtdeMaximaLinha = planilha.max_row
-    qtdeMaximaColuna = planilha.max_column
-    for i in range(1, qtdeMaximaLinha + 1):
-        lista = []
-        for j in range(1, qtdeMaximaColuna + 1):
-            lista.append(planilha.cell(row=i, column=j).value)
-        print(lista)
 
 if __name__ == '__main__':
-
     planilha = abrir_planilha('Contato.xlsx')
-
-   # planilha = abrir_planilha('gppInovacao.xlsx')
-
-    # print(type(planilha))
-
-    # mostrar o título da planilha
-    print('\nTitulo da planilha')
-    print(planilha.title)
-    print(planilha.dimensions)
-
-    # mostrar conteudo da planilha
-    print('\nMostrar conteudo da planilha')
-    # mostrar_conteudo_planilha(planilha)
