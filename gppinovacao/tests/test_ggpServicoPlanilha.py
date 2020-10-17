5e745455e58916baf140a21cf7c17ddd73c05d53
ogpgit <osvaldogpires@gmail.com>
2020-10-17 12:22:16 -0300

    Criar ambiente de teste e criar um módulo de exemplo de teste.
    close #12

--- gppinovacao/tests/test_ggpServicoPlanilha.py
+++ gppinovacao/tests/test_ggpServicoPlanilha.py
@@ -4,7 +4,7 @@ from gppinovacao.gppServicoPlanilha import abrir_planilha
 def test_int():
     assert 1 == 1
 
+
 def test_abrirPlanilha():
     planilha = abrir_planilha('Contato.xlsx')
     assert type(planilha) != "<class 'openpyxl.worksheet.worksheet.Worksheet'>"
-
