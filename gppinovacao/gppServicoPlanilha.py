cf154b49482a644f0a82ae82725cf768a4fe9204
ogpgit <osvaldogpires@gmail.com>
2020-10-18 11:32:23 -0300

    Implementando metodos de trabalho com planilha Excel.
    close #13

--- gppinovacao/gppServicoPlanilha.py
+++ gppinovacao/gppServicoPlanilha.py
@@ -1,6 +1,16 @@
 from openpyxl import load_workbook
 
 
+def path_planilha(nome_planilha):
+    """
+        Retornar o caminho completo da planilha
+
+    :param nome_planilha: str com o nome da planilha
+    :return: o caminho completo da planilha
+    """
+    return f'G:\\AmbientePython\\PycharmProjects\\gppinovacao\\{nome_planilha}'
+
+
 def abrir_planilha(nome_planilha):
     """
        Abrir uma planilha excel existente
@@ -8,8 +18,8 @@ def abrir_planilha(nome_planilha):
        :param nome_planilha: str com o nome da planilha
        :return: um objeto workbook ativado
        """
-    nome_planilha = f'G:\\AmbientePython\\PycharmProjects\\gppinovacao\\{nome_planilha}'
-    return load_workbook(nome_planilha).active
+    # nome_planilha = f'G:\\AmbientePython\\PycharmProjects\\gppinovacao\\{nome_planilha}'
+    return load_workbook(path_planilha(nome_planilha)).active
 
 
 def mostrar_conteudo_planilha(planilha):
@@ -22,29 +32,37 @@ def mostrar_conteudo_planilha(planilha):
         print(lista)
 
 
-def salvar_planilha(planilha):
-    planilha['A2'] = 'Osvaldo Gonzaga'
-    print(planilha.cell(2, 1).value)
-    # planilha.save('Contato.xlsx')
+def salvar_planilha(wb, path):
+    wb.save(path)
 
 
 if __name__ == '__main__':
+
     planilha = abrir_planilha('Contato.xlsx')
 
-    # planilha = abrir_planilha('gppInovacao.xlsx')
-
-    # print(type(planilha))
+    # Salvar conteudo da planilha
+    print('\nSalvar conteudo da planilha')
+    path = path_planilha('Contato.xlsx')
+    wb = load_workbook(path, False)
+    folha = wb['Plan1']
+    print(folha['A2'].value)
+    folha['A2'] = 'Osvaldo Gonzaga Pires'
+    salvar_planilha(wb, path)
+    wb.close()
 
     # mostrar o título da planilha
-    print('\nTitulo da planilha')
+    # planilha = abrir_planilha('gppInovacao.xlsx')
+    # print(type(planilha))
+    print('\nMostrar titulo da planilha')
     print(planilha.title)
     print(planilha.dimensions)
     print(type(planilha))
 
-    # mostrar conteudo da planilha
-    print('\nMostrar conteudo da planilha')
-    mostrar_conteudo_planilha(planilha)
-
-    # Salvar conteuda da planilha
-    print('\nSalvar conteudo da planilha')
-    # salvar_planilha(planilha)
+    # percorrer as folhas da planilha
+    path = path_planilha('Contato.xlsx')
+    folhas = load_workbook(path).sheetnames
+    for folha in folhas:
+        print(f'\nMostrar conteudos da folha {folha.title()}')
+        # print(folha.title())
+        pla = load_workbook(path)[folha]
+        mostrar_conteudo_planilha(pla)
