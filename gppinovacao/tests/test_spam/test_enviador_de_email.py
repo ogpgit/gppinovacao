68498248c5341980437dd3d761df9072fa1916aa
ogpgit <osvaldogpires@gmail.com>
2020-10-22 07:14:00 -0300

    Implementando o projeto utilizando a técnica de Desenvolvimento Orientado a Testes, do inglês Test Driven Development (TDD) bem como o chamado desenvolvido com Passos de Bebê, do inglês Baby Steps
    close #14

---
+++ gppinovacao/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,16 @@
+from gppinovacao.spam.enviador_de_email import Enviador
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+def test_remetente():
+    enviador = Enviador()
+    resultado = enviador.enviar(
+        'osvaldogpires@gmail.com',
+        'osvaldo-gp@detran.go.gov.br',
+        'Cursos Python Pro',
+        'Primeira turma do curso Python Pro'
+    )
+    assert 'osvaldogpires@gmail.com' in resultado
