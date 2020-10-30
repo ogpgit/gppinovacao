f2f6d29ba5a4c6764c1cd2803012c5abf67c911d
ogpgit <osvaldogpires@gmail.com>
2020-10-30 08:40:39 -0300

    Implementando Conexão, Sessão e Usuário
    close #21

--- gppinovacao/tests/test_spam/test_usuarios.py
+++ gppinovacao/tests/test_spam/test_usuarios.py
@@ -1,7 +1,11 @@
+from gppinovacao.spam.db import Conexao
+from gppinovacao.spam.modelos import Usuario
+
+
 def test_salvar_usuario():
     conexao = Conexao()
     sessao = conexao.gerar_sessao()
-    usuario = Usuario(nome = 'Osvaldo')
+    usuario = Usuario(nome='Osvaldo')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
     sessao.roll_back()
@@ -9,10 +13,10 @@ def test_salvar_usuario():
     conexao.fechar()
 
 
-def test_litar_usuarios():
+def test_listar_usuarios():
     conexao = Conexao()
     sessao = conexao.gerar_sessao()
-    usuarios = [Usuario(nome = 'Osvaldo'), Usuario(nome = 'Silvia')]
+    usuarios = [Usuario(nome='Osvaldo'), Usuario(nome='Silvia')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar_usuarios()
