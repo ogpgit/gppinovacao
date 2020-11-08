96775891bfd6c6558780147df4aca9574bc13d9e
ogpgit <osvaldogpires@gmail.com>
2020-11-08 07:55:25 -0300

    Implementando Mock - Nesse tópico você vai conferir como a lib mock funciona para economizar trabalho de escrita de testes.
    close #27

--- gppinovacao/tests/test_spam/teste_envio_para_base_de_usuarios.py
+++ gppinovacao/tests/test_spam/teste_envio_para_base_de_usuarios.py
@@ -1,6 +1,5 @@
 import pytest
-
-from gppinovacao.spam.enviador_de_email import Enviador
+from unittest.mock import Mock
 from gppinovacao.spam.main import EnviadorDeSpam
 from gppinovacao.spam.modelos import Usuario
 
@@ -20,38 +19,27 @@ from gppinovacao.spam.modelos import Usuario
 def test_qtde_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'osvaldogpires@gmail.com',
         'Curso Python Pro',
         'Confira os módulos fantaticos'
     )
-    assert len(usuarios) == enviador.qtde_email_enviados
-
-
-class EnviadorMock(Enviador):
-    def __init__(self):
-        super().__init__()
-        self.qtde_email_enviados = 0
-        self.parametros_de_envio = None
-
-    def enviar(self, remetente, destinatario, assunto, corpo):
-        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
-        self.qtde_email_enviados += 1
+    assert len(usuarios) == enviador.enviar.call_count
 
 
 def test_parametros_de_spam(sessao):
     usuario = Usuario(nome='Osvaldo', email='osvaldogpires@gmail.com')
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'silvia1875@gmail.com',
         'Curso Python Pro',
         'Confira os módulos fantaticos'
     )
-    assert enviador.parametros_de_envio == (
+    enviador.enviar.assert_called_once_with(
         'silvia1875@gmail.com',
         'osvaldogpires@gmail.com',
         'Curso Python Pro',
